6704fde4368d9dd1611c45771326427db0341ee8
Federico Knüssel <fknussel@gmail.com>
2015-01-06 23:46:55 -0300

    Update StatusContract with provider specific constants

--- app/src/main/java/com/fknussel/yamba/StatusContract.java
+++ app/src/main/java/com/fknussel/yamba/StatusContract.java
@@ -1,9 +1,12 @@
 package com.fknussel.yamba;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class StatusContract {
 
+    // DB specific constants
+    // ---------------------------
     // This is the actual SQLite file that will contain the database
     public static final String DB_NAME = "timeline.db";
     
@@ -26,4 +29,15 @@ public class StatusContract {
         public static final String MESSAGE = "message";
         public static final String CREATED_AT = "created_at";
     }
+    
+    // Provider specific constants
+    // ---------------------------
+    // content://com.fknussel.yamba.StatusProvider/status
+    public static final String AUTHORITY = "com.fknussel.yamba.StatusProvider";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE);
+    public static final int STATUS_ITEM = 1;
+    public static final int STATUS_DIR = 2;
+    public static final String STATUS_TYPE_ITEM = "vnd.android.cursor.item/vnd.com.fknussel.yamba.provider.status";
+    public static final String STATUS_TYPE_DIR = "vnd.android.cursor.dir/vnd.com.fknussel.yamba.provider.status";
+    
 }
