ee9092289cc70c64b55a8c14b18c707a9c67be14
Federico Knüssel <fknussel@gmail.com>
2015-01-06 23:57:18 -0300

    Update RefreshService to make use of our brand new content provider

--- app/src/main/java/com/fknussel/yamba/RefreshService.java
+++ app/src/main/java/com/fknussel/yamba/RefreshService.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
@@ -52,17 +53,19 @@ public class RefreshService extends IntentService {
             return;
         }
 
-        DbHelper dbHelper = new DbHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Log.d(TAG, "onStarted");
         
         // ContentValues is a simple data structure consisting of name-value pairs
         // that maps database table names to their respective values
         ContentValues values = new ContentValues();
 
-        YambaClient yambaCloud = new YambaClient(username, password);
+        YambaClient cloud = new YambaClient(username, password);
 
         try {
-            List<YambaClient.Status> timeline = yambaCloud.getTimeline(20);
+            int count = 0;
+            
+            List<YambaClient.Status> timeline = cloud.getTimeline(20);
+            
             for (YambaClient.Status status : timeline) {
                 // For each record, we create a content value. We are reusing 
                 // the same Java object, clearing it each time we start the loop and
@@ -72,14 +75,20 @@ public class RefreshService extends IntentService {
                 values.put(StatusContract.Column.USER, status.getUser());
                 values.put(StatusContract.Column.MESSAGE, status.getMessage());
                 values.put(StatusContract.Column.CREATED_AT, status.getCreatedAt().getTime());
-                
-                // Notice that we are not piecing together an SQL statement here, but
-                // rather using a prepared statement approach to inserting into the database
-                // We use insertWithOnConflict() instead of insert(), which allows us to specify
-                // what to do in case of a constract violation (e.g., duplicate ID).
-                // Our call passes the CONFLICT_IGNORE parameter to tell the database
-                // to just silently ignore our attempt to update it.
-                db.insertWithOnConflict(StatusContract.TABLE, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+
+                // Here we use getContentResolver() from the current context to get the access
+                // to content provider’s insert(). The actual provider to use is resolved via
+                // the URI that we pass: StatusContract.CONTENT_URI, which is registered with
+                // the system via the application AndroidManifest.xml file.
+                // This is how the content resolver knows that it’s StatusProvider
+                // on the receiving end of this insert() call.
+                Uri uri = getContentResolver().insert(StatusContract.CONTENT_URI, values);
+
+                if (uri != null) {
+                    // Count how many successful inserts we actually had
+                    count++;
+                    Log.d(TAG, String.format("%s: %s", status.getUser(), status.getMessage()));
+                }
             }
         } catch(YambaClientException e) {
             Log.e(TAG, "Failed to fetch timeline", e);
