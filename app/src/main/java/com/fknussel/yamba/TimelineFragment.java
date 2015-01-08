5ab038c4f480d3bfde616d05785751907da68437
Federico Knüssel <fknussel@gmail.com>
2015-01-08 09:00:37 -0300

    Use relative time instead of timestamps for displaying statuses' date

--- app/src/main/java/com/fknussel/yamba/TimelineFragment.java
+++ app/src/main/java/com/fknussel/yamba/TimelineFragment.java
@@ -6,8 +6,11 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 // We implement LoaderCallbacks<Cursor>, which is the set of callbacks
 // that will be called when the data is available. CursorLoader consists
@@ -44,11 +47,16 @@ public class TimelineFragment extends ListFragment implements LoaderManager.Load
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        setEmptyText("Loading data...");
+
         // Here, we create the adapter that glues together the data to the
         // custom view R.layout.list_item. It does that by binding the database
         // columns defined by the FROM array to view IDs identified by the TO array.
         mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item, null, FROM, TO, 0);
 
+        // We attach a custom ViewBinder instance to our stock adapter
+        mAdapter.setViewBinder(new TimelineViewBinder());
+        
         // We finally attach this adapter to the ListView that is already
         // embedded in the ListFragment, of which TimelineFragment is a subclass.
         setListAdapter(mAdapter);
@@ -91,5 +99,45 @@ public class TimelineFragment extends ListFragment implements LoaderManager.Load
     public void onLoaderReset(Loader<Cursor> loader) {
         // In case the data is stale or unavailable, we remove it from the view
         mAdapter.swapCursor(null);
+    }
+
+
+    /** The actual implementation of a ViewBinder instance that handles custom
+     * binding of data to view. Notice that we are implementing it as an inner class.
+     * There’s no reason for any other class to use it, and thus it shouldn’t
+     * be exposed to the outside world. Also notice that it is static final,
+     * meaning that it’s a constant.
+     * */
+    class TimelineViewBinder implements SimpleCursorAdapter.ViewBinder {
+
+        // This method gets called for each data element that
+        // needs to be bound to a particular view
+        @Override
+        public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+            
+            // First we check whether this view is the view we care about, i.e.,
+            // our TextView representing when the status was created.
+            // If not, we return false, which causes the adapter to handle the bind itself
+            // in the standard manner. If it is our view, we move on and do the custom bind.
+            if (view.getId() != R.id.list_item_text_created_at) {
+                return false;
+            }
+
+            // Convert timestamp to relative time
+            // We get the raw timestamp value from the cursor data
+            long timestamp = cursor.getLong(columnIndex);
+            
+            // Using the same Android helper method we used in our previous example,
+            // DateUtils.getRelativeTimeSpanString(), we convert the timestamp to a
+            // human-readable format. This is that business logic that we are injecting.
+            CharSequence relativeTime = DateUtils.getRelativeTimeSpanString(timestamp);
+            
+            // Update the text on the actual view
+            ((TextView) view).setText(relativeTime);
+            
+            // Return true so that SimpleCursorAdapter does not process bindView()
+            // on this element in its standard way
+            return true;
+        }
     }
 }
